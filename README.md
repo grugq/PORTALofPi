66dba4b862cc837b331c771cfc9e93cc83946fec
Douglas Goddard <dgoddard@securityevaluators.com>
2016-02-16 11:54:00 -0500

    Comments and better readme.

--- README.md
+++ README.md
@@ -5,12 +5,14 @@
  |_|  \___/|_|_\ |_/_/ \_\____| |
 ```
 
-PORTAL of Pi - RaspberyPi based PORTAL device. Certified UNIX Network Technicians only!
+PORTAL of Pi - RaspberyPi based PORTAL device. 
 
 Development and Design Guide
 =============================
 
 By: the grugq <thegrugq@gmail.com>
+Fork: @TACIXAT
+
 
 Guide
 =====
@@ -20,4 +22,28 @@ which transparently forwards all TCP traffic over the Tor network. There is
 also a Tor SOCKS proxy for explicitly interacting with the Tor network, either
 for more security, or to access a Hidden Service.
 
-Internet access is assumed to use a USB ethernet adapter.
+This fork is intended to do a little more heavy lifting than the original. 
+
+Requirements: 
+	USB ethernet adapter
+	Raspberry Pi 2
+	SD card
+	Computer
+
+sd.sh - Grab an SD card and run sd.sh /dev/sdX. This will format the SD card, 
+download the Arch image, load it, change the default passwords, and load the 
+init script (build.sh).
+
+build.sh - Boot your RaspberryPi. Connect to some internet (generally, just 
+plug into the ethernet jack). Login as alarm, su to a root shell. Run build.sh. 
+This should install necessary packages, config files, and services. Reboot.
+(Thanks grugq!) 
+
+This setup assumes usage of a USB ethernet adapter. Once setup your connection
+should look like:
+
+COMPUTER.ETH ---[]--- PI.USB.ETH -- PI -- PI.ETH ---[]--- INTERNET
+
+Hopefully we will add wifi support in the future, but this will be less of a
+plug-n-play solution (since you have to join networks, requires Pi login).
+
--- sd.sh
+++ sd.sh
@@ -1,4 +1,49 @@
 #!/bin/bash
+
+# Source: http://archlinuxarm.org/platforms/armv7/broadcom/raspberry-pi-2
+# Installation
+
+# SD Card Creation
+#     Replace sdX in the following instructions with the device name for the SD card as it appears on your computer.
+
+#     Start fdisk to partition the SD card:
+#         fdisk /dev/sdX
+
+#     At the fdisk prompt, delete old partitions and create a new one:
+#         Type o. This will clear out any partitions on the drive.
+#         Type p to list partitions. There should be no partitions left.
+#         Type n, then p for primary, 1 for the first partition on the drive, press ENTER to accept the default first sector, then type +100M for the last sector.
+#         Type t, then c to set the first partition to type W95 FAT32 (LBA).
+#         Type n, then p for primary, 2 for the second partition on the drive, and then press ENTER twice to accept the default first and last sector.
+#         Write the partition table and exit by typing w.
+    
+#     Create and mount the FAT filesystem:
+#         mkfs.vfat /dev/sdX1
+#         mkdir boot
+#         mount /dev/sdX1 boot
+
+#     Create and mount the ext4 filesystem:
+#         mkfs.ext4 /dev/sdX2
+#         mkdir root
+#         mount /dev/sdX2 root
+
+#     Download and extract the root filesystem (as root, not via sudo):
+#         wget http://archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz
+#         bsdtar -xpf ArchLinuxARM-rpi-2-latest.tar.gz -C root
+#         sync
+
+#     Move boot files to the first partition:
+#         mv root/boot/* boot
+
+#     Unmount the two partitions:
+#         umount boot root
+
+# Insert the SD card into the Raspberry Pi, connect ethernet, and apply 5V power.
+# Use the serial console or SSH to the IP address given to the board by your router.
+
+# Login as the default user alarm with the password alarm.
+# The default root password is root.
+
 if [ -z $1 ] 
 then
     echo "USAGE: $0 /dev/sdX"
@@ -6,7 +51,8 @@ then
 fi
 target=$1
 
-# do fdisk
+echo "=== FORMATTING DISK $0"
+
 echo "o
 n
 p
@@ -22,6 +68,7 @@ p
 
 w" | fdisk $target
 
+echo "=== MAKING FILESYSTEMS AND MOUNTING"
 mkfs.vfat $target"1"
 mkdir boot
 mount $target"1" boot
@@ -30,18 +77,21 @@ mkfs.ext4 $target"2"
 mkdir root
 mount $target"2" root
 
+echo "=== DOWNLOADING IMAGE"
 wget http://archlinuxarm.org/os/ArchLinuxARM-rpi-2-latest.tar.gz
 bsdtar -xpf ArchLinuxARM-rpi-2-latest.tar.gz -C root
 
-echo "SYNC -- THIS MIGHT TAKE SOME MINUTES"
+echo "=== SYNCING -- THIS MIGHT TAKE SOME MINUTES"
 sync
 
 ls root/boot
 rsync -abmv --remove-source-files root/boot/* boot
 find root/boot -depth -type d -empty -delete
 
+echo "=== COPYING BUILD SCRIPT TO /home/alarm"
 cp build.sh root/home/alarm/
 
+echo "=== CHANGE PASSWORDS"
 SALT=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 echo -n "Update alarm's password: "
 read -s PASS
@@ -56,9 +106,10 @@ echo
 HASH=$(mkpasswd  -m sha-512 -S $SALT -s <<< $PASS)
 sed -i "s~root:[^:]\+:~root:$HASH:~" root/etc/shadow
 
-echo "SYNC -- JUST TO BE SAFE"
+echo "=== SYNCING -- JUST TO BE SAFE"
 sync
 
+echo "=== CLEANUP"
 umount boot root
 rm -r boot root
 rm ArchLinuxARM-rpi-2-latest.tar.gz
